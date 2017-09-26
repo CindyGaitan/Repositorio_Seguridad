2baab4dcf1d942eb817b4d3830f7a66046e74ed8
CindyGaitan <cindygaitan54@gmail.com>
2017-09-25 23:16:27 -0600

    Agregacion de form cambio de contraseña, usuario y aplicacion.

--- PrototipoSeguridad/Aplicaciones.cs
+++ PrototipoSeguridad/Aplicaciones.cs
@@ -15,7 +15,7 @@ namespace PrototipoSeguridad
     public partial class Aplicaciones : Form
     {
         MySqlConnection con;
-        MySqlCommand com;
+        MySqlCommand com, com2;
         MySqlDataAdapter da;
         DataTable dt;
         MySqlDataReader dr;
@@ -23,17 +23,8 @@ namespace PrototipoSeguridad
         public Aplicaciones()
         {
             InitializeComponent();
-            try
-            {
-                con = new MySqlConnection("server = localhost; user id = root; password=1234; database = BD_seguridad");
-                con.Open();
-                MessageBox.Show("conectado");
-                mostrar_aplicacion();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("No conectado");
-            }
+            mostrar_aplicacion();
+            
         }
 
         private void Aplicaciones_Load(object sender, EventArgs e)
@@ -45,40 +36,11 @@ namespace PrototipoSeguridad
         {
             try
             {
-                int check1 = 1;
-                int check2 = 2;
-                int check3 = 3;
+                con = new MySqlConnection("server = localhost; user id = root; database = BD_seguridad");
+                con.Open();
 
                 com = new MySqlCommand("insert into aplicacion (nombre_aplicacion, descripcion_aplicacion) values ('"+ txt_aplicacion.Text +"', '"+ txt_aplicacion_descripcion.Text +"')", con);
-               com.ExecuteNonQuery();
-               MessageBox.Show("Datos Ingresados");
-
-                string id_aplicacion;
-                int id = 0;
-                com = new MySqlCommand("select id_aplicacion from aplicacion where nombre_aplicacion='" + txt_aplicacion.Text + "' ", con);
-                dr = com.ExecuteReader();
-                while (dr.Read())
-                {
-                    id_aplicacion = dr["id_aplicacion"].ToString();
-                    id = Convert.ToInt32(id_aplicacion);
-                }
-                dr.Close();
-
-                if (chb_insertar.Checked)
-                {
-                    com = new MySqlCommand("insert into detalle_aplicacion_derecho (id_aplicacion, id_derecho) values("+ id +", "+ check1 +")", con);
-                    com.ExecuteNonQuery();
-                }
-                if (chb_eliminar.Checked)
-                {
-                    com = new MySqlCommand("insert into detalle_aplicacion_derecho (id_aplicacion, id_derecho) values(" + id + ", " + check2 + ")", con);
-                    com.ExecuteNonQuery();
-                }
-                if (chb_modificar.Checked)
-                {
-                    com = new MySqlCommand("insert into detalle_aplicacion_derecho (id_aplicacion, id_derecho) values(" + id + ", " + check3 + ")", con);
-                    com.ExecuteNonQuery();
-                }
+                com.ExecuteNonQuery();
 
                 MessageBox.Show("Datos Ingresados");
                 mostrar_aplicacion();
@@ -94,6 +56,9 @@ namespace PrototipoSeguridad
 
         public void mostrar_aplicacion()
         {
+            con = new MySqlConnection("server = localhost; user id = root; database = BD_seguridad");
+            con.Open();
+
             da = new MySqlDataAdapter("select *from aplicacion", con);
             dt = new DataTable();
             da.Fill(dt);
@@ -104,12 +69,15 @@ namespace PrototipoSeguridad
         {
             try
             {
-                com = new MySqlCommand("update aplicacion set nombre_aplicacion = " + txt_aplicacion.Text + "descripcio_aplicacion = " + txt_aplicacion_descripcion.Text, con);
+                con = new MySqlConnection("server = localhost; user id = root; database = BD_seguridad");
+                con.Open();
+
+                com = new MySqlCommand("update aplicacion set nombre_aplicacion='"+ txt_aplicacion.Text +"', descripcion_aplicacion='"+ txt_aplicacion_descripcion.Text +"' where id_aplicacion="+ Convert.ToInt32(this.var_aplicacion) +" ", con);
                 com.ExecuteNonQuery();
 
+                mostrar_aplicacion();
                 txt_aplicacion.Text = "";
                 txt_aplicacion_descripcion.Text = "";
-                mostrar_aplicacion();
                 MessageBox.Show("Datos actualizados");
             }
             catch (Exception ex)
@@ -123,17 +91,50 @@ namespace PrototipoSeguridad
         {
             try
             {
-                com = new MySqlCommand("delete from aplicacion where nombre_aplicacion=" + txt_aplicacion.Text + " ", con);
+                con = new MySqlConnection("server = localhost; user id = root; database = BD_seguridad");
+                con.Open();
+
+                string id_aplicacion;
+                int id = 0;
+                com = new MySqlCommand("select id_aplicacion from aplicacion where nombre_aplicacion='" + txt_aplicacion.Text + "' ", con);
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    id_aplicacion = dr["id_aplicacion"].ToString();
+                    id = Convert.ToInt32(id_aplicacion);
+                }
+                dr.Close();
+
+                com = new MySqlCommand("delete from aplicacion where id_aplicacion=" + id + " ", con);
                 com.ExecuteNonQuery();
+
                 txt_aplicacion.Text = "";
                 txt_aplicacion_descripcion.Text = "";
-
                 mostrar_aplicacion();
                 MessageBox.Show("Registro eliminado correctamente");
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error al eliminar, dato no puede ser eliminado");
+            }
+        }
+
+        string var_aplicacion;
+        private void dgv_aplicacion_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                DataGridViewRow dgv = dgv_aplicacion.Rows[e.RowIndex];
+
+                this.var_aplicacion = dgv.Cells[0].Value.ToString();
+                txt_aplicacion.Text = dgv.Cells[1].Value.ToString();
+                txt_aplicacion_descripcion.Text = dgv.Cells[2].Value.ToString();
+               
+            }
+            catch (Exception ex)
+
+            {
+                MessageBox.Show("ERROR");
             }
         }
     }
