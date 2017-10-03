b7aafba4f65fb87938e3db6d51b0498b227269d1
CindyGaitan <cindygaitan54@gmail.com>
2017-10-03 00:18:48 -0600

    Cmbios a la Base de datos, perfil Unique, agregacion de Form Perfil.

--- PrototipoSeguridad/Perfiles.cs
+++ PrototipoSeguridad/Perfiles.cs
@@ -7,55 +7,158 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
+using System.Data.Odbc;
 
 
 namespace PrototipoSeguridad
 {
     public partial class Perfiles : Form
     {
-        MySqlConnection con;
-        MySqlCommand com;
-        MySqlDataAdapter da;
+        OdbcCommand com;
+        OdbcDataAdapter da;
         DataTable dt;
+        OdbcDataReader dr;
+        Conexion con = new Conexion();
 
         public Perfiles()
-        {          
+        {
             InitializeComponent();
-            try
-            {
-                con = new MySqlConnection("server = localhost; user id = root; password=1234; database = BD_seguridad");
-                con.Open();
-                MessageBox.Show("conectado");
-                mostrar_perfil();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("No conectado");
-            }
+            mostrar_perfil();
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            com = new MySqlCommand("insert into perfil (nombre_perfil, descripcion_perfil) values ('" + Txt_nombre.Text + "', '" + Txt_descripcion.Text + "')", con);
-            com.ExecuteNonQuery();
 
-            MessageBox.Show("Datos Ingresados");
-            mostrar_perfil();
         }
 
         public void mostrar_perfil()
         {
-            da = new MySqlDataAdapter("select *from perfil", con);
-            dt = new DataTable();
-            da.Fill(dt);
-            dgv_perfil.DataSource = dt;
+            try
+            {
+                da = new OdbcDataAdapter("select p.id_perfil as Num,p.nombre_perfil as nombre, descripcion_perfil as descripcion from perfil p", con.conexion());
+                dt = new DataTable();
+                da.Fill(dt);
+                dgv_perfil.DataSource = dt;
+                dgv_perfil.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR");
+            }
         }
-
 
         private void Btn_modificar_Click(object sender, EventArgs e)
         {
 
+        }
+        string var_id;
+        private void dgv_perfil_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string id_perfil; int id = 0;
+                com = new OdbcCommand("select id_perfil from perfil where nombre_perfil ='" + Txt_nombre.Text + "' ", con.conexion());
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    id_perfil = dr["id_perfil"].ToString();
+                    id = Convert.ToInt32(id_perfil);
+                }
+                dr.Close();
+
+                if (id == 0)
+                {
+
+                    com = new OdbcCommand("insert into Perfil ( nombre_perfil, descripcion_perfil) values ('" + Txt_nombre.Text + "','" + Txt_descripcion.Text + "')", con.conexion());
+                    com.ExecuteNonQuery();
+                    mostrar_perfil();
+                    Txt_nombre.Text = ""; Txt_nombre.Text = ""; Txt_descripcion.Text = "";
+                    MessageBox.Show("Datos Ingresados");
+                }
+                else
+                {
+                    MessageBox.Show("Nombre de perfil ya existe, ingrese uno distinto.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Datos NO ingresados, verifique la información. "+ex.ToString());
+            }
+        }
+
+   
+      private void btn_editar_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            try
+            {
+                try
+                {
+                    com = new OdbcCommand("update perfil set nombre_perfil='" + Txt_nombre.Text + "' where id_perfil=" + Convert.ToInt32(this.var_id), con.conexion());
+                    com.ExecuteNonQuery();
+                }
+                catch (Exception exe)
+                {
+                    MessageBox.Show("Perfil existente, ingrese uno nuevo");
+                    i = 1;
+                }
+
+                com = new OdbcCommand("update perfil set nombre_perfil='" + Txt_nombre.Text + "', descripcion_perfil='" + Txt_descripcion.Text + "' where id_perfil=" + Convert.ToInt32(this.var_id), con.conexion());
+                com.ExecuteNonQuery();
+                mostrar_perfil();
+                Txt_nombre.Text = ""; Txt_descripcion.Text = "";
+                MessageBox.Show("Datos actualizados.");
+            }
+            catch (Exception ex)
+            {
+                if (i == 1)
+                {
+                }
+                else
+                {
+                    MessageBox.Show("Datos NO actualizados, verifique información.");
+                }
+            }
+        }
+
+        private void btn_borrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(this.var_id);
+                string eliminar = "delete from perfil where id_perfil = " + id + " ";
+                com = new OdbcCommand(eliminar, con.conexion());
+                com.ExecuteNonQuery();
+                mostrar_perfil();
+                Txt_nombre.Text = ""; Txt_descripcion.Text = "";
+                MessageBox.Show("Registro eliminado correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Datos NO eliminados.");
+            }
+        }
+
+        private void dgv_perfil_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+            try
+            {
+                DataGridViewRow dgv = dgv_perfil.Rows[e.RowIndex];
+                this.var_id = dgv.Cells[0].Value.ToString();
+                Txt_nombre.Text = dgv.Cells[1].Value.ToString();
+                Txt_descripcion.Text = dgv.Cells[2].Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR");
+            }
         }
     }
 }
--- PrototipoSeguridad/Perfiles.designer.cs
+++ PrototipoSeguridad/Perfiles.designer.cs
@@ -34,14 +34,16 @@
             this.Txt_descripcion = new System.Windows.Forms.TextBox();
             this.Txt_nombre = new System.Windows.Forms.TextBox();
             this.Lbl_nombre = new System.Windows.Forms.Label();
-            this.Gpb_accion = new System.Windows.Forms.GroupBox();
-            this.Btn_eliminar = new System.Windows.Forms.Button();
-            this.Btn_listo = new System.Windows.Forms.Button();
-            this.Btn_modificar = new System.Windows.Forms.Button();
-            this.Btn_agregar = new System.Windows.Forms.Button();
+            this.Pnl_botones = new System.Windows.Forms.Panel();
+            this.btn_borrar = new System.Windows.Forms.Button();
+            this.btn_editar = new System.Windows.Forms.Button();
+            this.Lbl_borrar = new System.Windows.Forms.Label();
+            this.Lbl_editar = new System.Windows.Forms.Label();
+            this.Lbl_guardar = new System.Windows.Forms.Label();
+            this.btn_guardar = new System.Windows.Forms.Button();
             this.dgv_perfil = new System.Windows.Forms.DataGridView();
             this.Gpb_datos.SuspendLayout();
-            this.Gpb_accion.SuspendLayout();
+            this.Pnl_botones.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_perfil)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,17 +53,17 @@
             this.Gpb_datos.Controls.Add(this.Txt_descripcion);
             this.Gpb_datos.Controls.Add(this.Txt_nombre);
             this.Gpb_datos.Controls.Add(this.Lbl_nombre);
-            this.Gpb_datos.Location = new System.Drawing.Point(52, 45);
+            this.Gpb_datos.Location = new System.Drawing.Point(10, 198);
             this.Gpb_datos.Name = "Gpb_datos";
             this.Gpb_datos.Size = new System.Drawing.Size(294, 202);
-            this.Gpb_datos.TabIndex = 0;
+            this.Gpb_datos.TabIndex = 20;
             this.Gpb_datos.TabStop = false;
             this.Gpb_datos.Text = "DATOS DEL PERFIL";
             // 
             // Lbl_descripcion
             // 
             this.Lbl_descripcion.AutoSize = true;
-            this.Lbl_descripcion.Location = new System.Drawing.Point(13, 77);
+            this.Lbl_descripcion.Location = new System.Drawing.Point(17, 87);
             this.Lbl_descripcion.Name = "Lbl_descripcion";
             this.Lbl_descripcion.Size = new System.Drawing.Size(63, 13);
             this.Lbl_descripcion.TabIndex = 5;
@@ -69,7 +71,7 @@
             // 
             // Txt_descripcion
             // 
-            this.Txt_descripcion.Location = new System.Drawing.Point(97, 74);
+            this.Txt_descripcion.Location = new System.Drawing.Point(101, 84);
             this.Txt_descripcion.Multiline = true;
             this.Txt_descripcion.Name = "Txt_descripcion";
             this.Txt_descripcion.Size = new System.Drawing.Size(182, 78);
@@ -77,7 +79,7 @@
             // 
             // Txt_nombre
             // 
-            this.Txt_nombre.Location = new System.Drawing.Point(97, 39);
+            this.Txt_nombre.Location = new System.Drawing.Point(101, 49);
             this.Txt_nombre.Name = "Txt_nombre";
             this.Txt_nombre.Size = new System.Drawing.Size(182, 20);
             this.Txt_nombre.TabIndex = 3;
@@ -85,104 +87,119 @@
             // Lbl_nombre
             // 
             this.Lbl_nombre.AutoSize = true;
-            this.Lbl_nombre.Location = new System.Drawing.Point(13, 42);
+            this.Lbl_nombre.Location = new System.Drawing.Point(17, 52);
             this.Lbl_nombre.Name = "Lbl_nombre";
             this.Lbl_nombre.Size = new System.Drawing.Size(70, 13);
             this.Lbl_nombre.TabIndex = 1;
             this.Lbl_nombre.Text = "Nombre Perfil";
             // 
-            // Gpb_accion
+            // Pnl_botones
             // 
-            this.Gpb_accion.Controls.Add(this.Btn_eliminar);
-            this.Gpb_accion.Controls.Add(this.Btn_listo);
-            this.Gpb_accion.Controls.Add(this.Btn_modificar);
-            this.Gpb_accion.Controls.Add(this.Btn_agregar);
-            this.Gpb_accion.Location = new System.Drawing.Point(352, 45);
-            this.Gpb_accion.Name = "Gpb_accion";
-            this.Gpb_accion.Size = new System.Drawing.Size(287, 202);
-            this.Gpb_accion.TabIndex = 1;
-            this.Gpb_accion.TabStop = false;
-            this.Gpb_accion.Text = "ACCIONES";
+            this.Pnl_botones.BackColor = System.Drawing.Color.LightBlue;
+            this.Pnl_botones.Controls.Add(this.btn_borrar);
+            this.Pnl_botones.Controls.Add(this.btn_editar);
+            this.Pnl_botones.Controls.Add(this.Lbl_borrar);
+            this.Pnl_botones.Controls.Add(this.Lbl_editar);
+            this.Pnl_botones.Controls.Add(this.Lbl_guardar);
+            this.Pnl_botones.Controls.Add(this.btn_guardar);
+            this.Pnl_botones.Location = new System.Drawing.Point(207, 39);
+            this.Pnl_botones.Name = "Pnl_botones";
+            this.Pnl_botones.Size = new System.Drawing.Size(283, 106);
+            this.Pnl_botones.TabIndex = 19;
             // 
-            // Btn_eliminar
+            // btn_borrar
             // 
-            this.Btn_eliminar.FlatAppearance.BorderSize = 0;
-            this.Btn_eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_eliminar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_eliminar.Image")));
-            this.Btn_eliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Btn_eliminar.Location = new System.Drawing.Point(11, 115);
-            this.Btn_eliminar.Name = "Btn_eliminar";
-            this.Btn_eliminar.Size = new System.Drawing.Size(130, 65);
-            this.Btn_eliminar.TabIndex = 3;
-            this.Btn_eliminar.Text = "Eliminar";
-            this.Btn_eliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_borrar.FlatAppearance.BorderSize = 0;
+            this.btn_borrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_borrar.Image = ((System.Drawing.Image)(resources.GetObject("btn_borrar.Image")));
+            this.btn_borrar.Location = new System.Drawing.Point(199, 7);
+            this.btn_borrar.Name = "btn_borrar";
+            this.btn_borrar.Size = new System.Drawing.Size(65, 65);
+            this.btn_borrar.TabIndex = 16;
+            this.btn_borrar.UseVisualStyleBackColor = true;
+            this.btn_borrar.Click += new System.EventHandler(this.btn_borrar_Click);
             // 
-            // Btn_listo
+            // btn_editar
             // 
-            this.Btn_listo.FlatAppearance.BorderSize = 0;
-            this.Btn_listo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_listo.Image = ((System.Drawing.Image)(resources.GetObject("Btn_listo.Image")));
-            this.Btn_listo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Btn_listo.Location = new System.Drawing.Point(147, 118);
-            this.Btn_listo.Name = "Btn_listo";
-            this.Btn_listo.Size = new System.Drawing.Size(130, 65);
-            this.Btn_listo.TabIndex = 2;
-            this.Btn_listo.Text = "Aceptar";
-            this.Btn_listo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Btn_listo.UseVisualStyleBackColor = true;
+            this.btn_editar.FlatAppearance.BorderSize = 0;
+            this.btn_editar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_editar.Image = ((System.Drawing.Image)(resources.GetObject("btn_editar.Image")));
+            this.btn_editar.Location = new System.Drawing.Point(111, 7);
+            this.btn_editar.Name = "btn_editar";
+            this.btn_editar.Size = new System.Drawing.Size(65, 65);
+            this.btn_editar.TabIndex = 13;
+            this.btn_editar.UseVisualStyleBackColor = true;
+            this.btn_editar.Click += new System.EventHandler(this.btn_editar_Click);
             // 
-            // Btn_modificar
+            // Lbl_borrar
             // 
-            this.Btn_modificar.FlatAppearance.BorderSize = 0;
-            this.Btn_modificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_modificar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_modificar.Image")));
-            this.Btn_modificar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Btn_modificar.Location = new System.Drawing.Point(147, 19);
-            this.Btn_modificar.Name = "Btn_modificar";
-            this.Btn_modificar.Size = new System.Drawing.Size(130, 65);
-            this.Btn_modificar.TabIndex = 1;
-            this.Btn_modificar.Text = "Modificar";
-            this.Btn_modificar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Btn_modificar.UseVisualStyleBackColor = true;
-            this.Btn_modificar.Click += new System.EventHandler(this.Btn_modificar_Click);
+            this.Lbl_borrar.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_borrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_borrar.Location = new System.Drawing.Point(199, 75);
+            this.Lbl_borrar.Name = "Lbl_borrar";
+            this.Lbl_borrar.Size = new System.Drawing.Size(65, 22);
+            this.Lbl_borrar.TabIndex = 10;
+            this.Lbl_borrar.Text = "Borrar";
+            this.Lbl_borrar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // Btn_agregar
+            // Lbl_editar
             // 
-            this.Btn_agregar.FlatAppearance.BorderSize = 0;
-            this.Btn_agregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_agregar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_agregar.Image")));
-            this.Btn_agregar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Btn_agregar.Location = new System.Drawing.Point(11, 19);
-            this.Btn_agregar.Name = "Btn_agregar";
-            this.Btn_agregar.Size = new System.Drawing.Size(130, 65);
-            this.Btn_agregar.TabIndex = 0;
-            this.Btn_agregar.Text = "Agregar";
-            this.Btn_agregar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Btn_agregar.UseVisualStyleBackColor = true;
-            this.Btn_agregar.Click += new System.EventHandler(this.button1_Click);
+            this.Lbl_editar.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_editar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_editar.Location = new System.Drawing.Point(111, 75);
+            this.Lbl_editar.Name = "Lbl_editar";
+            this.Lbl_editar.Size = new System.Drawing.Size(65, 22);
+            this.Lbl_editar.TabIndex = 7;
+            this.Lbl_editar.Text = "Editar";
+            this.Lbl_editar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Lbl_guardar
+            // 
+            this.Lbl_guardar.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_guardar.Location = new System.Drawing.Point(21, 75);
+            this.Lbl_guardar.Name = "Lbl_guardar";
+            this.Lbl_guardar.Size = new System.Drawing.Size(65, 22);
+            this.Lbl_guardar.TabIndex = 1;
+            this.Lbl_guardar.Text = "Guardar";
+            this.Lbl_guardar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.FlatAppearance.BorderSize = 0;
+            this.btn_guardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_guardar.Image = ((System.Drawing.Image)(resources.GetObject("btn_guardar.Image")));
+            this.btn_guardar.Location = new System.Drawing.Point(21, 7);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(65, 65);
+            this.btn_guardar.TabIndex = 0;
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
             // 
             // dgv_perfil
             // 
             this.dgv_perfil.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_perfil.Location = new System.Drawing.Point(52, 265);
+            this.dgv_perfil.Location = new System.Drawing.Point(310, 198);
             this.dgv_perfil.Name = "dgv_perfil";
-            this.dgv_perfil.Size = new System.Drawing.Size(587, 161);
-            this.dgv_perfil.TabIndex = 2;
+            this.dgv_perfil.Size = new System.Drawing.Size(358, 195);
+            this.dgv_perfil.TabIndex = 18;
+            this.dgv_perfil.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_perfil_CellContentClick_1);
             // 
             // Perfiles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(679, 438);
-            this.Controls.Add(this.dgv_perfil);
-            this.Controls.Add(this.Gpb_accion);
             this.Controls.Add(this.Gpb_datos);
+            this.Controls.Add(this.Pnl_botones);
+            this.Controls.Add(this.dgv_perfil);
             this.Name = "Perfiles";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PERFILES";
             this.Gpb_datos.ResumeLayout(false);
             this.Gpb_datos.PerformLayout();
-            this.Gpb_accion.ResumeLayout(false);
+            this.Pnl_botones.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgv_perfil)).EndInit();
             this.ResumeLayout(false);
 
@@ -195,11 +212,13 @@
         private System.Windows.Forms.TextBox Txt_descripcion;
         private System.Windows.Forms.TextBox Txt_nombre;
         private System.Windows.Forms.Label Lbl_nombre;
-        private System.Windows.Forms.GroupBox Gpb_accion;
-        private System.Windows.Forms.Button Btn_eliminar;
-        private System.Windows.Forms.Button Btn_listo;
-        private System.Windows.Forms.Button Btn_modificar;
-        private System.Windows.Forms.Button Btn_agregar;
+        private System.Windows.Forms.Panel Pnl_botones;
+        private System.Windows.Forms.Button btn_borrar;
+        private System.Windows.Forms.Button btn_editar;
+        private System.Windows.Forms.Label Lbl_borrar;
+        private System.Windows.Forms.Label Lbl_editar;
+        private System.Windows.Forms.Label Lbl_guardar;
+        private System.Windows.Forms.Button btn_guardar;
         private System.Windows.Forms.DataGridView dgv_perfil;
     }
 }
